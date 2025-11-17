d73ba3ff84644dc9beaf60f3547cd2774734b558
鬼影233 <82418456+gui-ying233@users.noreply.github.com>
2025-11-17 18:00:36 -0500

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,1 +1,27 @@
+// ==UserScript==
+// @name         纽约客查看更多
+// @namespace    https://github.com/gui-ying233/MoreFreeTNY
+// @version      1.0.0
+// @description  去除纽约客的订阅提示并显示更多内容（不一定能完全显示）
+// @author       鬼影233
+// @license      MIT
+// @match        https://www.newyorker.com/*
+// @icon         https://www.newyorker.com/verso/static/thenewyorker-us/assets/favicon.ico
+// @supportURL   https://github.com/gui-ying233/MoreFreeTNY/issues
+// @run-at       document-start
+// ==/UserScript==
 
+(() => {
+	"use strict";
+	const originalRemove = Element.prototype.remove;
+	Element.prototype.remove = function (...args) {
+		if (this.tagName === "FIGURE" || this.classList.contains("paywall") || this.querySelector(".paywall, figure")) return;
+		return originalRemove.apply(this, args);
+	};
+	document.head.appendChild(
+		Object.assign(document.createElement("style"), {
+			textContent:
+				'.journey-unit__container{display:none}',
+		})
+	);
+})();
